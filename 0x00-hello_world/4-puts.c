1bbb47dab5444e7cfc4aaa82897cd9a37f965a65
daking24 <watcher24.inc@gmail.com>
2023-02-16 21:35:25 +0100

    Task 4: Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,15 +5,15 @@
 
 #include <stdio.h>
 
-/*
+/**
  * main - Prints "Programming is like building a multilingual
- *                puzzle" followed by a new line.
+ *                puzzle, followed by a new line.
  *
  * Return: 0 - Always.
  */
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
