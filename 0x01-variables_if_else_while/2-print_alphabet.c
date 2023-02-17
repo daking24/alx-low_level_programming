2c96075098d5950ae5d57a739b8b22851ded9dc9
daking24 <watcher24.inc@gmail.com>
2023-02-17 21:24:30 +0100

    Task 2: I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: King David Ajayi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabets in descending order from a-z.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char low_alphabet;
+
+for (low_alphabet = 'a'; low_alphabet <= 'z'; low_alphabet++)
+{
+putchar(low_alphabet);
+putchar('\n');
+
+return (0);
+}
