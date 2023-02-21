a7bd21c3b662d14c6169f00dc47487148b2d616a
daking24 <watcher24.inc@gmail.com>
2023-02-21 23:17:02 +0100

    Task 2: 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,9 @@ void print_alphabet_x10(void)
 {
 int x;
 char lower;
-for(x = 1; x <= 10; x++)
+for (x = 1; x <= 10; x++)
 {
-for (lower='a'; lower <= 'z'; lower++)
+for (lower ='a'; lower <= 'z'; lower++)
 {
 _putchar(lower);
 }
