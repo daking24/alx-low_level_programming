33c7840b21e7a5fdec3d18b4535d043062b00ed2
daking24 <watcher24.inc@gmail.com>
2023-02-21 20:56:15 +0100

    Task 0: trial 1

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: King David Ajayi
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
+#endif
