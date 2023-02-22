fa68b7e6630b809ad870c6479f35a9f0f2551547
daking24 <watcher24.inc@gmail.com>
2023-02-23 00:52:02 +0100

    Task 3: Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,29 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+int days_in_feb = 28;
 if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 {
-if (month >= 3 && day >= 60)
+days_in_feb = 29;
+}
+
+if (month >= 2 && day > days_in_feb)
 {
 day++;
 }
 
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+int day_of_year = convert_day(month, day);
+if (month <= 0 || month > 12 || day <= 0 || day > 31)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+}
+else if (month == 2 && day == 29 && days_in_feb == 28)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 }
 else
 {
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", 365 + (days_in_feb == 29) - day_of_year);
 }
 }
