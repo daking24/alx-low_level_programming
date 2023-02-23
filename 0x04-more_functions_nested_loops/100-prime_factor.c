bfd8d656a35567a285571a28080ac29c40e55ba9
daking24 <watcher24.inc@gmail.com>
2023-02-24 00:03:42 +0100

    100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,16 +1,34 @@
 /*
- * File: <name>
+ * File: 100-prime_factor.c
  * Auth: King David Ajayi
  */
 
-#include "main.h"
+#include <stdio.h>
 
 /**
- * func - desc
- * [input]
+ * main - Finds and prints the largest prime
+ *        factor of the number 612852475143.
  *
- * Return:
+ * Return: Always 0.
  */
-void main(void)
+int main(void)
 {
+long prime = 612852475143, div;
+while (div < (prime / 2))
+{
+if ((prime % 2) == 0)
+{
+prime /= 2;
+continue;
+}
+
+for (div = 3; div < (prime / 2); div += 2)
+{
+if ((prime % div) == 0)
+prime /= div;
+}
+}
+
+printf("%ld\n", prime);
+return (0);
 }
