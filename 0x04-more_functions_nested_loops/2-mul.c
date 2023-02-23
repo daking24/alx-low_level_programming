66c0b29e2bbce5a402dc2c72a01741625f034479
daking24 <watcher24.inc@gmail.com>
2023-02-23 21:15:18 +0100

    Task 2 done.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Auth: King David Ajayi
+ */
+ 
+ #include "main.h"
+ 
+ /**
+  * mul - Multiply 2 integers
+  * @a: First integer
+  * @b: Second integer
+  *
+  * Return:
+  */
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
