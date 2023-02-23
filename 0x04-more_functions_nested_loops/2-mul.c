da542426c3c586be8bd052940af9c2367d887a28
daking24 <watcher24.inc@gmail.com>
2023-02-23 22:02:03 +0100

    CHECKERgit add .!

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,18 +2,20 @@
  * File: 2-mul.c
  * Auth: King David Ajayi
  */
- 
-#include "main.h"
 
+#include "main.h"
 /**
- * mul - multiplies two integers
- *
- * @a: first integer
- * @b: second integer
- *
- * Return: the result of multiplication
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
  */
 int mul(int a, int b)
 {
-return (a * b);
+int mul;
+
+mul = a * b;
+
+return (mul);
 }
+
