4d54f1502828d7f99e8ccb040cb05fa55474abb7
daking24 <watcher24.inc@gmail.com>
2023-02-23 23:43:51 +0100

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,15 +20,15 @@ for (num = 1; num <= 100; num++)
 {
 if ((num % 3) == 0)
 {
-printf("Fizz");
+printf(" Fizz");
 }
 else if ((num % 5) == 0)
 {
-printf("Buzz");
+printf(" Buzz");
 }
 else if ((num % 3) == 0 && (num % 5) == 0)
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
 }
 else if (i == 1)
 {
@@ -36,7 +36,7 @@ printf("%d", i);
 }
 else
 {
-printf("%d", i);
+printf(" %d", i);
 }
 }
 printf("\n");
