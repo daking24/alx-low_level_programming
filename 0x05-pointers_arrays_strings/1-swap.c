017dead8a3fb48cdfc8d94c1420f0279df81eab8
daking24 <watcher24.inc@gmail.com>
2023-02-27 23:10:09 +0100

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/*
+ * File: 1-swap.c
+ * Auth: King David Ajayi
+ */
+
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ */
+void swap_int(int *a, int *b) {
+int temp = *a;
+*a = *b;
+*b = temp;
+}
