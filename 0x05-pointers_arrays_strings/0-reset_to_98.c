68fc90dfe4719e4ef2a9fe817d46aa89217f6d95
daking24 <watcher24.inc@gmail.com>
2023-02-27 22:53:14 +0100

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: King David Ajayi
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an 'int' as parameter
+ *               and updates the value it points to to '98'.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: King David Ajayi
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
