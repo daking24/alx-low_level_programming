46d47b07c669fdac2512997f31b84fda0e933d3e
daking24 <watcher24.inc@gmail.com>
2023-02-28 00:20:06 +0100

    Task 4 update x1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,13 @@
 void print_rev(char *s)
 {
 int len = 0, i;
-while (*s != '\0')
+while (s[i++])
 {
 len++;
-s++;
 }
 for (i = len - 1; i >= 0; i--)
 {
-_putchar(*(s - i - 1));
+_putchar(s[i]);
 }
 _putchar('\n');
 }
