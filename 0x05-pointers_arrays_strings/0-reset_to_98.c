ac6c8b4266372bb1a23e68d2e6139e4c9483311a
daking24 <watcher24.inc@gmail.com>
2023-02-27 22:57:00 +0100

    Task 0 update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,6 +8,7 @@
 /**
  * reset_to_98 - Takes a pointer to an 'int' as parameter
  *               and updates the value it points to to '98'.
+ * @n: The pointer to an int. 
  */
 void reset_to_98(int *n)
 {
