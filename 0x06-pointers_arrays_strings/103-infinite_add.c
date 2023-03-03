cec26a387a4c97c8de43a394b17c03bebdc93f1f
daking24 <watcher24.inc@gmail.com>
2023-03-03 03:26:54 +0100

    Fixes x1

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -59,35 +59,25 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-{
 val1 = *(n1 + i) -'0';
-}
 if (j < 0)
 val2 = 0;
 else
-{
 val2 = *(n2 + j) -'0';
-}
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
 else
-{
 overflow = 0;
-}
 if (digits >= (size_r - 1))
-{
 return (0);
-}
 *(r + digits) = (temp_tot % 10) + '0';
 digits++;
 j--;
 i--;
 }
 if (digits == size_r)
-{
 return (0);
-}
 *(r + digits) = '\0';
 rev_string(r);
 return (r);
