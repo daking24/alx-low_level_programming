aefe69db3333627e3821da17c04945b8338563e5
daking24 <watcher24.inc@gmail.com>
2023-03-03 01:10:25 +0100

    Task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: King David King
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x06-pointers-arrays_strings directory.
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
