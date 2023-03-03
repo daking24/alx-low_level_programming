b7fcb8c04d72d7ad1772fb4b5e403c5f9812c798
daking24 <watcher24.inc@gmail.com>
2023-03-03 03:24:13 +0100

    Fixes

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -45,9 +45,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 int overflow = 0, i = 0, j = 0, digits = 0;
 int val1 = 0, val2 = 0, temp_tot = 0;
 while (*(n1 + i) != '\0')
-{
 i++;
-}
 while (*(n2 + j) != '\0')
 {
 j++;
@@ -55,32 +53,24 @@ j++;
 i--;
 j--;
 if (j >= size_r || i >= size_r)
-{
 return (0);
-}
 while (j >= 0 || i >= 0 || overflow == 1)
 {
 if (i < 0)
-{
 val1 = 0;
-}
 else
 {
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i) -'0';
 }
 if (j < 0)
-{
 val2 = 0;
-}
 else
 {
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j) -'0';
 }
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
-{
 overflow = 1;
-}
 else
 {
 overflow = 0;
