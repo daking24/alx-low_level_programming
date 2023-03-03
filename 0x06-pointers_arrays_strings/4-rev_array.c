e6e4441395a02957d673a790e9c537f8535a6911
daking24 <watcher24.inc@gmail.com>
2023-03-03 02:41:05 +0100

    Mandatory tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+/*
+ * File: 4-rev_array.c
+ * Auth: King David Ajayi
+ */
+
+#include "main.h"
+
+/**
+ * reverse_array - reverse array elements
+ * @a: array
+ * @n: number of elements in array
+ */
+void reverse_array(int *a, int n)
+{
+int tmp, beg = 0;
+int end = n - 1;
+while (beg < end)
+{
+tmp = *(a + beg);
+*(a + beg) = *(a + end);
+*(a + end) = tmp;
+beg++, end--;
+}
+}
