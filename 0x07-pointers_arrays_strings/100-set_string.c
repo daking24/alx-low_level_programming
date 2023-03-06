ca45b2123f0a193a7ceb449ecde8fce9b741fb24
daking24 <watcher24.inc@gmail.com>
2023-03-07 00:29:06 +0100

    Advanced tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+/*
+ * File: 100-set_string.c
+ * Auth: King David Ajayi
+ */
+
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
