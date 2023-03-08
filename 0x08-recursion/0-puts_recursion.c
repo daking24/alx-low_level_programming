3986bd8b66ac705786a452d61789a7fbae918011
daking24 <watcher24.inc@gmail.com>
2023-03-09 00:04:10 +0100

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-puts_recursion.c
+ * Auth: King David Ajayi
+ */
+
+#include "main.h"
+
+/**
+ * _puts_recursion - print string with new line
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+s++;
+_puts_recursion(s);
+}
