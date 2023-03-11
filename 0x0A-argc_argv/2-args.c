a84de077aed63718cd691927d1e1a3a5ef3cd11c
daking24 <watcher24.inc@gmail.com>
2023-03-11 01:08:59 +0100

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-args.c
+ * Auth: King David Ajayi
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print each argument passed to program on a line
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+int = i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
