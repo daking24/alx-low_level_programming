3d2c1b68863f432b1aa022cc40394837479ce426
daking24 <watcher24.inc@gmail.com>
2023-03-11 01:19:57 +0100

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+/*
+ * File: 3-mul.c
+ * Auth: King David Ajayi
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print product of argument numbers
+ * @argc: argument counter
+ * @argv: numbers to multiply
+ * Return: 0 on success, 1 if two arguments not given
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+}
