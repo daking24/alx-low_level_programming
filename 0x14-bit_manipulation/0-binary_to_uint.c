0d2de80c3886948911bc248e4652ffb2322d4451
daking24 <watcher24.inc@gmail.com>
2023-04-06 15:12:16 +0100

    WD: Task Completed

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,42 @@
+/*
+ * Auth: King David Ajayi
+ * File: 0-binary_to_uint.c
+ */
+
+#include "main.h"
+
+/**
+ * binary_to_uint - Transforms a binary number to an unsigned int.
+ * @b: A pointer to string of 0 and 1 chars.
+ *
+ * Return: If b = NULL or contains not 0 or 1 - 0.
+ * 	   Else - the converted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number = 0, mult = 1;
+	int len;
+
+	if (b == '\0')
+	{
+		return (0);
+	}
+
+	for (len = 0; b[len];)
+	{
+		len++;
+	}
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		number += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (number);
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+/*
+ * Auth: King David Ajayi
+ * File: 3-set_bit.c
+ */
+
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of bit at a given index to 1.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value.
+ *
+ * Return: If error occurs - (-1).
+ * 	   Else - 1.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	*n ^= (1 << index);
+
+	return (1);
+}
